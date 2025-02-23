29b7d1a94d9d9cfea3c0ebf0db9fac64447691d3
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-02-23 11:46:11 +0800

    initial commit

---
+++ src/test/java/com/gregperlinli/certvault/CertVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregperlinli.certvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
