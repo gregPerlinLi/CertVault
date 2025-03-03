a2e4a52fb4ba0ce63567089441d4e1bf6ff1dbfc
SNRainiar <rainiar@foxmail.com>
2025-03-03 21:05:25 +0800

    chore: burnt out everything

--- frontend/postcss.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'postcss-load-config';
-
-// Export configs
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-} satisfies Config;
--- frontend/src/views/Login.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-const test = ref("Login");
-</script>
-
-<template>
-    <Button>{{ test }}</Button>
-</template>
