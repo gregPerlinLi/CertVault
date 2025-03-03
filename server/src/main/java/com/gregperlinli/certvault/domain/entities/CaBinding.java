263a673f2b7d334f18722dd94a0de7ec3413a7ff
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-03 22:34:49 +0800

    feat(domain): Add entity classes related to CA, roles, and users
    - Add Ca class for CA list
    - Add CaBinding class for assigning CA users
    - Add Role class for permission management
    - Add RoleBinding class for permission binding
    - Add User class for user management

---
+++ server/src/main/java/com/gregperlinli/certvault/domain/entities/CaBinding.java
@@ -0,0 +1,50 @@
+package com.gregperlinli.certvault.domain.entities;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+/**
+ * <p>
+ * 分配 CA 用户
+ * </p>
+ *
+ * @author gregPerlinLi
+ * @since 2025-03-03
+ */
+@Getter
+@Setter
+@ToString
+@TableName("ca_bindings")
+public class CaBinding implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 分配ID
+     */
+    @TableId("id")
+    private Integer id;
+
+    /**
+     * CA ID 
+     */
+    @TableField("ca_id")
+    private Integer caId;
+
+    /**
+     * 用户ID
+     */
+    @TableField("uid")
+    private Integer uid;
+
+    /**
+     * 创建时间
+     */
+    @TableField("created_at")
+    private LocalDateTime createdAt;
+}
---
+++ server/src/main/java/com/gregperlinli/certvault/domain/entities/RoleBinding.java
@@ -0,0 +1,50 @@
+package com.gregperlinli.certvault.domain.entities;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+/**
+ * <p>
+ * 权限绑定
+ * </p>
+ *
+ * @author gregPerlinLi
+ * @since 2025-03-03
+ */
+@Getter
+@Setter
+@ToString
+@TableName("role_bindings")
+public class RoleBinding implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户ID
+     */
+    @TableField("uid")
+    private Integer uid;
+
+    /**
+     * 权限ID
+     */
+    @TableField("role_id")
+    private Integer roleId;
+
+    /**
+     * 创建时间
+     */
+    @TableField("created_at")
+    private Integer createdAt;
+}
