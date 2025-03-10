f866efe6e61caf599d313e0d95eb93af132d33bc
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-10 21:44:23 +0800

    feat(user): Refactor user service and add new features
    - Refactor the login feature, return LoginResultDTO instead of UserDTO
    - Add features such as login verification, logout, retrieving user information, and updating user information
    - Optimize code structure, improve maintainability and scalability

--- server/src/main/java/com/gregperlinli/certvault/service/impl/UserServiceImpl.java
+++ server/src/main/java/com/gregperlinli/certvault/service/impl/UserServiceImpl.java
@@ -3,14 +3,14 @@ package com.gregperlinli.certvault.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.gregperlinli.certvault.constant.RedisKeyConstant;
 import com.gregperlinli.certvault.constant.ResultStatusCodeConstant;
-import com.gregperlinli.certvault.domain.dto.UserDTO;
-import com.gregperlinli.certvault.domain.entities.Role;
+import com.gregperlinli.certvault.domain.dto.LoginResultDTO;
+import com.gregperlinli.certvault.domain.dto.UpdateUserProfileDTO;
+import com.gregperlinli.certvault.domain.dto.UserProfileDTO;
 import com.gregperlinli.certvault.domain.entities.RoleBinding;
 import com.gregperlinli.certvault.domain.entities.User;
 import com.gregperlinli.certvault.domain.exception.ParamValidateException;
-import com.gregperlinli.certvault.mappers.RoleBindingMapper;
-import com.gregperlinli.certvault.mappers.RoleMapper;
-import com.gregperlinli.certvault.mappers.UserMapper;
+import com.gregperlinli.certvault.mapper.RoleBindingMapper;
+import com.gregperlinli.certvault.mapper.UserMapper;
 import com.gregperlinli.certvault.service.interfaces.IUserService;
 import com.gregperlinli.certvault.utils.*;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -32,17 +32,14 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
-    @Resource
-    StringRedisTemplate redisTemplate;
+    @Resource()
+    StringRedisTemplate stringRedisTemplate;
 
     @Resource
     RoleBindingMapper roleBindingMapper;
 
-    @Resource
-    RoleMapper roleMapper;
-
     @Override
-    public UserDTO login(String username, String password, String sessionId) {
+    public LoginResultDTO login(String username, String password, String sessionId) {
         if ( !GenericUtils.allOfNullable(username, password) ) {
             throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The parameter cannot be null.");
         }
@@ -50,15 +47,69 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         userQueryWrapper.eq("username", username);
         User user = this.getOne(userQueryWrapper);
         if ( user != null && AuthUtils.matchesPassword(password, user.getPassword()) ) {
-            redisTemplate.opsForValue().set(RedisKeyConstant.USER.joinLoginPrefix(sessionId), user.getUsername(), 60, TimeUnit.MINUTES);
-            QueryWrapper<RoleBinding> roleBindingQueryWrapper = new QueryWrapper<>();
-            roleBindingQueryWrapper.eq("uid", user.getId());
-            List<RoleBinding> roleBindings = roleBindingMapper.selectList(roleBindingQueryWrapper);
-            QueryWrapper<Role> roleQueryWrapper = new QueryWrapper<>();
-            roleQueryWrapper.in("id", roleBindings.stream().map(RoleBinding::getRoleId).toList());
-            List<Role> roles = roleMapper.selectList(roleQueryWrapper);
-            return new UserDTO(user, roles);
+            stringRedisTemplate.opsForValue().set(RedisKeyConstant.USER.joinLoginPrefix(sessionId), user.getUsername(), 60, TimeUnit.MINUTES);
+            return new LoginResultDTO(user);
         }
         return null;
+    }
+
+    @Override
+    public boolean loginVerify(String username, String sessionId) {
+        if ( !GenericUtils.ofNullable(sessionId) ) {
+            throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The parameter cannot be empty.");
+        }
+        String loginUser = stringRedisTemplate.opsForValue().get(RedisKeyConstant.USER.joinLoginPrefix(sessionId));
+        return username.equals(loginUser);
+    }
+
+    @Override
+    public void logout(String sessionId) {
+        if ( !GenericUtils.ofNullable(sessionId) ) {
+            throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The parameter cannot be empty.");
+        }
+        stringRedisTemplate.delete(RedisKeyConstant.USER.joinLoginPrefix(sessionId));
+    }
+
+    @Override
+    public UserProfileDTO getOwnProfile(String username) {
+        if ( !GenericUtils.ofNullable(username) ) {
+            throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The parameter cannot be empty.");
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", username);
+        User user = this.getOne(queryWrapper);
+        if ( user != null ) {
+            return new UserProfileDTO(user);
+        }
+        throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The user does not exist.");
+    }
+
+    @Override
+    public Boolean updateOwnProfile(String username, UpdateUserProfileDTO updateUserProfileDTO) {
+        if ( !GenericUtils.ofNullable(updateUserProfileDTO.getDisplayName()) ) {
+            throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The parameter cannot be null.");
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", username);
+        User user = this.getOne(queryWrapper);
+        if ( user != null ) {
+            if ( GenericUtils.ofNullable(updateUserProfileDTO.getOldPassword()) || GenericUtils.ofNullable(updateUserProfileDTO.getNewPassword()) ) {
+                if ( !AuthUtils.matchesPassword(updateUserProfileDTO.getOldPassword(), user.getPassword())) {
+                    throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The old password is incorrect.");
+                } else {
+                    user.setPassword(AuthUtils.encryptPassword(updateUserProfileDTO.getNewPassword()));
+                }
+            }
+            user.setDisplayName(updateUserProfileDTO.getDisplayName());
+            return this.updateById(user);
+        }
+        throw new ParamValidateException(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), "The user does not exist.");
+    }
+
+    private List<Integer> getRoleBindings(User user) {
+        QueryWrapper<RoleBinding> roleBindingQueryWrapper = new QueryWrapper<>();
+        roleBindingQueryWrapper.eq("uid", user.getId());
+        List<RoleBinding> roleBindings = roleBindingMapper.selectList(roleBindingQueryWrapper);
+        return roleBindings.stream().map(RoleBinding::getRoleId).toList();
     }
 }
--- server/src/main/java/com/gregperlinli/certvault/service/interfaces/IUserService.java
+++ server/src/main/java/com/gregperlinli/certvault/service/interfaces/IUserService.java
@@ -1,6 +1,8 @@
 package com.gregperlinli.certvault.service.interfaces;
 
-import com.gregperlinli.certvault.domain.dto.UserDTO;
+import com.gregperlinli.certvault.domain.dto.LoginResultDTO;
+import com.gregperlinli.certvault.domain.dto.UpdateUserProfileDTO;
+import com.gregperlinli.certvault.domain.dto.UserProfileDTO;
 import com.gregperlinli.certvault.domain.entities.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -22,6 +24,37 @@ public interface IUserService extends IService<User> {
      * @param sessionId sessionId
      * @return login result
      */
-    UserDTO login(String username, String password, String sessionId);
+    LoginResultDTO login(String username, String password, String sessionId);
+
+    /**
+     * 登录校验
+     *
+     * @param username 用户名
+     * @param sessionId 登录会话ID
+     * @return 校验结果
+     */
+    boolean loginVerify(String username, String sessionId);
+
+    /**
+     * 注销账户
+     *
+     * @param sessionId 登录会话ID
+     */
+    void logout(String sessionId);
+
+    /**
+     * 获取自己的用户信息
+     *
+     * @param username 用户名
+     */
+    UserProfileDTO getOwnProfile(String username);
+
+    /**
+     * 更新自己的用户信息
+     *
+     * @param username 用户名
+     * @param updateUserProfileDTO 更新用户信息DTO
+     */
+    Boolean updateOwnProfile(String username, UpdateUserProfileDTO updateUserProfileDTO);
 
 }
