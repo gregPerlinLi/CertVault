058cc6c266b6c320c5895e0a92beebd633608086
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-10 21:39:22 +0800

    feat(domain): Add user configuration data transfer object
    - Added UserProfileDTO class for user configuration-related data transfer
    - Contains properties such as username, display name, and role
    - Provides a constructor for converting from the User entity class to UserProfileDTO

---
+++ server/src/main/java/com/gregperlinli/certvault/domain/dto/UserProfileDTO.java
@@ -0,0 +1,44 @@
+package com.gregperlinli.certvault.domain.dto;
+
+import com.gregperlinli.certvault.domain.entities.User;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * User Profile DTO
+ *
+ * @author gregPerlinLi
+ * @version 1.0.0
+ * @className {@code UserProfileDTO}
+ * @date 2025/3/10 20:22
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+public class UserProfileDTO {
+
+    /**
+     * username
+     */
+    private String username;
+
+    /**
+     * display name
+     */
+    private String displayName;
+
+    /**
+     * roles
+     */
+    private Integer role;
+
+    public UserProfileDTO(User user) {
+        this.username = user.getUsername();
+        this.displayName = user.getDisplayName();
+        this.role = user.getRole();
+    }
+
+}
