c110473b3b2f9b945b262c13ed1cf61ad0e9c60b
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-10 21:37:24 +0800

    feat(security): Add Spring Security configuration and set global interceptors
    - Add the LoginInterceptor interceptor in WebConfig to handle user login-related requests
    - Add the WebSecurityConfig class to configure Spring Security
    - Open all request paths globally and disable CSRF protection

---
+++ server/src/main/java/com/gregperlinli/certvault/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.gregperlinli.certvault.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Spring Security Config
+ *
+ * @author gregPerlinLi
+ * @version 1.0.0
+ * @className {@code WebSecurityConfig}
+ * @date 2025/3/10 16:15
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 允许所有请求通过
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().permitAll() // 全局开放所有路径
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+}
