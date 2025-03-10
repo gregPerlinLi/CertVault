1c632b0f8379c807be02476a494d0978c5925b18
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-10 21:34:26 +0800

    refactor(server): Refactor Mapper-related code
    - Update type-aliases-package to com.gregperlinli.certvault.domain.entities
    - Delete global-config configuration
    - Rename the mappers package to mapper
    - Update the MapperScan annotation in CertVaultApplication and MyBatisConfig

--- server/src/main/java/com/gregperlinli/certvault/mapper/UserMapper.java
+++ server/src/main/java/com/gregperlinli/certvault/mapper/UserMapper.java
@@ -1,4 +1,4 @@
-package com.gregperlinli.certvault.mappers;
+package com.gregperlinli.certvault.mapper;
 
 import com.gregperlinli.certvault.domain.entities.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
