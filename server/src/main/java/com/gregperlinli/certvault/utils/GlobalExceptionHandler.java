fc92cc2bd719c42ea9040508166a8eeedf41d5c6
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-10 21:41:05 +0800

    feat(server): Added global exception handler
    - Added GlobalExceptionHandler class for unified handling of various exceptions
    - Handles exceptions including login, authorization, parameter validation, business logic, resource not found, method not allowed, and server errors
    - Sets corresponding HTTP status codes and error messages based on different types of exceptions - Optimized exception handling logic, improving system stability and maintainability

---
+++ server/src/main/java/com/gregperlinli/certvault/utils/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.gregperlinli.certvault.utils;
+
+import com.gregperlinli.certvault.constant.GeneralConstant;
+import com.gregperlinli.certvault.constant.ResultStatusCodeConstant;
+import com.gregperlinli.certvault.domain.exception.*;
+import com.gregperlinli.certvault.domain.vo.ResultVO;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+/**
+ * 全局异常处理器
+ *
+ * @author gregPerlinLi
+ * @version 1.0.0
+ * @className GlobalExceptionHandler
+ * @date 2024/1/31 16:49
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResultVO<Void> exceptionHandler(HttpServletResponse response, Exception e){
+        String msg = e.getMessage();
+        if ( e instanceof LoginException) {
+            // response.setStatus(ResultStatusCodeConstant.FORBIDDEN);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.UNAUTHORIZED.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.UNAUTHORIZED.getResultCode(), msg);
+        }
+        if ( e instanceof PermissionException) {
+            // response.setStatus(ResultStatusCodeConstant.FORBIDDEN);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.FORBIDDEN.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.FORBIDDEN.getResultCode(), msg);
+        }
+        if ( e instanceof ParamValidateException) {
+            // response.setStatus(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.PARAM_VALIDATE_EXCEPTION.getResultCode(), msg);
+        }
+        if ( e instanceof BusinessException) {
+            // response.setStatus(ResultStatusCodeConstant.BUSINESS_EXCEPTION);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.BUSINESS_EXCEPTION.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.BUSINESS_EXCEPTION.getResultCode(), msg);
+        }
+        if ( e instanceof NoResourceFoundException) {
+            // response.setStatus(ResultStatusCodeConstant.FORBIDDEN);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.PAGE_NOT_FIND.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.PAGE_NOT_FIND.getResultCode(), msg);
+        }
+        if ( e instanceof MethodNotAllowedException || e instanceof NoSuchMethodException ) {
+            // response.setStatus(ResultStatusCodeConstant.FORBIDDEN);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.METHOD_NOT_ALLOWED.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.METHOD_NOT_ALLOWED.getResultCode(), msg);
+        }
+        if ( e instanceof ServerException) {
+            // response.setStatus(ResultStatusCodeConstant.SERVER_ERROR);
+            response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.SERVER_ERROR.getResultCode()));
+            return new ResultVO<>(ResultStatusCodeConstant.SERVER_ERROR.getResultCode(), msg);
+        }
+        // response.setStatus(ResultStatusCodeConstant.SERVER_ERROR);
+        e.printStackTrace();
+        response.setHeader(GeneralConstant.STATUS_CODE.getValue(), String.valueOf(ResultStatusCodeConstant.SERVER_ERROR.getResultCode()));
+        return new ResultVO<>(ResultStatusCodeConstant.SERVER_ERROR.getResultCode(), "Internal Server Error");
+    }
+}
