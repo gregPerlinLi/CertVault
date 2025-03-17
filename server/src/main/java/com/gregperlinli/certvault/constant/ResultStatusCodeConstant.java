5341705e7dd7df7948d066f13023e0850a90a76e
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-17 16:36:43 +0800

    refactor: Optimize the capitalization of error messages and unify the style - Change the first letter of all error messages to uppercase
    - Unify the format of error messages, such as "Page not find" instead of "page not find"
    - Adjust the wording of some error messages to make them more accurate and consistent

--- server/src/main/java/com/gregperlinli/certvault/constant/ResultStatusCodeConstant.java
+++ server/src/main/java/com/gregperlinli/certvault/constant/ResultStatusCodeConstant.java
@@ -16,7 +16,7 @@ public enum ResultStatusCodeConstant {
     /**
      * 请求成功
      */
-    SUCCESS(200, "success"),
+    SUCCESS(200, "Success"),
     /**
      * 处理结果为空
      */
@@ -24,11 +24,11 @@ public enum ResultStatusCodeConstant {
     /**
      * 未登录
      */
-    UNAUTHORIZED(401, "未登录"),
+    UNAUTHORIZED(401, "Unauthorized"),
     /**
      * 页面未找到
      */
-    PAGE_NOT_FIND(404, "page not find"),
+    PAGE_NOT_FIND(404, "Page not find"),
     /**
      * 方法不支持
      */
@@ -36,27 +36,27 @@ public enum ResultStatusCodeConstant {
     /**
      * 无权限访问
      */
-    FORBIDDEN(403, "无权限访问"),
+    FORBIDDEN(403, "Forbidden"),
     /**
      * 状态异常
      */
-    STATUS_EXCEPTION(417, "status exception"),
+    STATUS_EXCEPTION(417, "Status exception"),
     /**
      * 请求失败
      */
-    FAILED(444, "failed"),
+    FAILED(444, "Failed"),
     /**
      * 参数校验异常 (参数不合法)
      */
-    PARAM_VALIDATE_EXCEPTION(422, "param validate exception"),
+    PARAM_VALIDATE_EXCEPTION(422, "Param validate exception"),
     /**
      * 处理过程报错
      */
-    SERVER_ERROR(500, "server error"),
+    SERVER_ERROR(500, "Server error"),
     /**
      * 业务异常
      */
-    BUSINESS_EXCEPTION(501, "business exception");
+    BUSINESS_EXCEPTION(501, "Business exception");
 
     final int resultCode;
 
