3f48bf1307ae0f90c8a932166a4a339548e4127c
SNRainiar <rainiar@foxmail.com>
2025-03-17 10:35:26 +0800

    style(dashboard): Adjust divider borders for themes
    - Update divider border styles in Certificates, Profile, Settings, and Users views
    - Add light mode border color while preserving dark mode appearance
    - Ensure consistent styling across dashboard sections

--- frontend/src/views/dashboard/Certificates.vue
+++ frontend/src/views/dashboard/Certificates.vue
@@ -6,5 +6,5 @@ useTitle("Certificates - CertVault");
   <h1 class="font-bold text-2xl">
     <i class="mr-2 pi pi-verified text-xl"></i>Certificates
   </h1>
-  <hr class="border-2 border-neutral-500 my-2" />
+  <hr class="border-2 border-neutral-200 dark:border-neutral-500 my-2" />
 </template>
--- frontend/src/views/dashboard/Users.vue
+++ frontend/src/views/dashboard/Users.vue
@@ -6,5 +6,5 @@ useTitle("Users - CertVault");
   <h1 class="font-bold text-2xl">
     <i class="mr-2 pi pi-users text-xl"></i>Users
   </h1>
-  <hr class="border-2 border-neutral-500 my-2" />
+  <hr class="border-2 border-neutral-200 dark:border-neutral-500 my-2" />
 </template>
