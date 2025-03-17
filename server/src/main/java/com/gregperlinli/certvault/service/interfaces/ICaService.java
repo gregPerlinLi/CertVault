1b902f0b56bc4779a8fce42466426a315f7a94d3
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-18 00:31:17 +0800

    feat(service): Implement CA certificate related service methods
    - Add getCas method to get the list of CA certificates
    - Implement getCaCert method to get a single CA certificate
    - Add getCaPrivKey method to get the CA private key
    - Implement updateCaComment method to update the CA certificate remarks
    - Add requestCa method to request the creation of a new CA certificate
    - Implement renewCa method to renew the CA certificate
    - Add deleteCa method to delete the CA certificate

--- server/src/main/java/com/gregperlinli/certvault/service/interfaces/ICaService.java
+++ server/src/main/java/com/gregperlinli/certvault/service/interfaces/ICaService.java
@@ -1,5 +1,9 @@
 package com.gregperlinli.certvault.service.interfaces;
 
+import com.gregperlinli.certvault.domain.dto.CaInfoDTO;
+import com.gregperlinli.certvault.domain.dto.PageDTO;
+import com.gregperlinli.certvault.domain.dto.RequestCertDTO;
+import com.gregperlinli.certvault.domain.dto.ResponseCaDTO;
 import com.gregperlinli.certvault.domain.entities.Ca;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +16,75 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2025-03-03
  */
 public interface ICaService extends IService<Ca> {
+
+    /**
+     * Get all ca certificates
+     *
+     * @param owner owner of the ca certificates
+     * @param page page number
+     * @param limit page size
+     * @return ca certificates info
+     */
+    PageDTO<CaInfoDTO> getCas(String owner, Integer page, Integer limit);
+
+    /**
+     * Get ca certificate
+     *
+     * @param uuid ca certificate uuid
+     * @param owner owner of the ca certificate
+     * @return ca certificate
+     */
+    String getCaCert(String uuid, String owner);
+
+    /**
+     * Get ca private key
+     *
+     * @param uuid ca private key uuid
+     * @param owner owner of the ca private key
+     * @param password password of the ca private key
+     * @return ca private key
+     * @throws Exception decryption error
+     */
+    String getCaPrivKey(String uuid, String owner, String password) throws Exception;
+
+    /**
+     * Update ca comment
+     *
+     * @param uuid ca certificate uuid
+     * @param owner owner of the ca certificate
+     * @param comment new comment
+     * @return true if update successfully
+     */
+    Boolean updateCaComment(String uuid, String owner, String comment);
+
+    /**
+     * Request new ca certificate
+     *
+     * @param requestCertDTO new ca certificate info
+     * @param owner        owner of the new ca certificate
+     * @return new ca certificate info
+     * @throws Exception encryption error
+     */
+    ResponseCaDTO requestCa(RequestCertDTO requestCertDTO, String owner) throws Exception;
+
+    /**
+     * Renew an existing ca certificate
+     *
+     * @param oldCaUuid old ca certificate uuid
+     * @param expiry new expiry date
+     * @param owner owner of the new ca certificate
+     * @return renewed ca certificate info
+     * @throws Exception decryption error
+     */
+    ResponseCaDTO renewCa(String oldCaUuid, Integer expiry, String owner) throws Exception;
+
+    /**
+     * Delete an existing ca certificate
+     *
+     * @param uuid ca certificate uuid
+     * @param owner owner of the ca certificate
+     * @return true if delete successfully
+     */
+    Boolean deleteCa(String uuid, String owner);
 
 }
