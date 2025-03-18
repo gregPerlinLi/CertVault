7075a7b288fefe421526afbb815355c119862d8b
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-19 00:59:29 +0800

    feat(server): Add user management and CA binding related interfaces
    - Add an interface to retrieve all user information
    - Add an interface to modify the availability of CA
    - Add interfaces for binding and unbinding CA certificates to users
    - Optimize the return results of the interface to retrieve CA information
    - Correct the parameter passing of the interface to retrieve CA private keys

--- server/src/main/java/com/gregperlinli/certvault/controller/AdminController.java
+++ server/src/main/java/com/gregperlinli/certvault/controller/AdminController.java
@@ -3,7 +3,9 @@ package com.gregperlinli.certvault.controller;
 import com.gregperlinli.certvault.constant.ResultStatusCodeConstant;
 import com.gregperlinli.certvault.domain.dto.*;
 import com.gregperlinli.certvault.domain.vo.ResultVO;
+import com.gregperlinli.certvault.service.interfaces.ICaBindingService;
 import com.gregperlinli.certvault.service.interfaces.ICaService;
+import com.gregperlinli.certvault.service.interfaces.IUserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +23,30 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
 
     @Resource
+    IUserService userService;
+
+    @Resource
     ICaService caService;
+
+    @Resource
+    ICaBindingService caBindingService;
+
+    /**
+     * Get all users
+     *
+     * @param page the page number
+     * @param limit the limit of the page
+     * @return the result
+     */
+    @GetMapping(value = "/users/{page}/{limit}")
+    public ResultVO<PageDTO<UserProfileDTO>> getAllUsers(@PathVariable("page") Integer page,
+                                                         @PathVariable("limit") Integer limit) {
+        PageDTO<UserProfileDTO> result = userService.getAllUsers(page, limit);
+        if ( result != null && result.getList() != null && !result.getList().isEmpty()) {
+            return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success", result);
+        }
+        return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND.getResultCode(), "No data", result);
+    }
 
     /**
      * Get all CA information
@@ -39,7 +64,7 @@ public class AdminController {
         if ( result != null && result.getList() != null ) {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success", result);
         }
-        return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND.getResultCode(), "No data");
+        return new ResultVO<>(ResultStatusCodeConstant.NOT_FIND.getResultCode(), "No data", result);
     }
 
     /**
@@ -68,9 +93,9 @@ public class AdminController {
      * @throws Exception if the decrypt is failed
      */
     @PostMapping(value = "/cert/ca_privkey")
-    public ResultVO<String> getCaPrivKey(@RequestBody RequestPrivkeyDTO requestPrivkeyDTO,
+    public ResultVO<String> getCaPrivkey(@RequestBody RequestPrivkeyDTO requestPrivkeyDTO,
                                          HttpServletRequest request) throws Exception {
-        String result = caService.getCaPrivKey(requestPrivkeyDTO.getUuid(), request.getSession().getAttribute("username").toString(), requestPrivkeyDTO.getPassword());
+        String result = caService.getCaPrivKey(requestPrivkeyDTO, request.getSession().getAttribute("username").toString());
         if ( result != null ) {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success", result);
         }
@@ -92,6 +117,23 @@ public class AdminController {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success");
         }
         return new ResultVO<>(ResultStatusCodeConstant.FAILED.getResultCode(), "Failed");
+    }
+
+    /**
+     * Modify a CA availability
+     *
+     * @param uuid the uuid of the CA
+     * @param request the request
+     * @return the result
+     */
+    @PatchMapping(value = "/cert/ca_available/{uuid}")
+    public ResultVO<Boolean> modifyCaAvailable(@PathVariable("uuid") String uuid,
+                                            HttpServletRequest request) {
+        Boolean result = caService.modifyCaAvailability(uuid, request.getSession().getAttribute("username").toString());
+        if ( result ) {
+            return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Enabled", true);
+        }
+        return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Disabled", false);
     }
 
     /**
@@ -143,6 +185,36 @@ public class AdminController {
     public ResultVO<Void> deleteCa(@PathVariable("uuid") String uuid,
                                    HttpServletRequest request) {
         Boolean result = caService.deleteCa(uuid, request.getSession().getAttribute("username").toString());
+        if ( result ) {
+            return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success");
+        }
+        return new ResultVO<>(ResultStatusCodeConstant.FAILED.getResultCode(), "Failed");
+    }
+
+    /**
+     * Bind a CA certificate to a user
+     *
+     * @param caBindingDTO the CA binding DTO
+     * @return the result
+     */
+    @PostMapping(value = "/cert/bind_ca")
+    public ResultVO<Void> bindCaToUser(@RequestBody CaBindingDTO caBindingDTO) {
+        Boolean result = caBindingService.newBinding(caBindingDTO);
+        if ( result ) {
+            return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success");
+        }
+        return new ResultVO<>(ResultStatusCodeConstant.FAILED.getResultCode(), "Failed");
+    }
+
+    /**
+     * Unbind a CA certificate from a user
+     *
+     * @param caBindingDTO the CA binding DTO
+     * @return the result
+     */
+    @DeleteMapping(value = "/cert/bind_ca")
+    public ResultVO<Void> unbindCaFromUser(@RequestBody CaBindingDTO caBindingDTO) {
+        Boolean result = caBindingService.deleteBinding(caBindingDTO);
         if ( result ) {
             return new ResultVO<>(ResultStatusCodeConstant.SUCCESS.getResultCode(), "Success");
         }
