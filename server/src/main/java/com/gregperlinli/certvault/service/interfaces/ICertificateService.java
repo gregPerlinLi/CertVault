b117ff07c4d2fe3b3510d1c3afb466dd30cb3ce5
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-19 00:59:50 +0800

    feat(service): Implement SSL certificate-related service methods
    - New functions for retrieving certificate lists, retrieving individual certificates, and requesting certificates
    - Implement encryption and decryption of certificate private keys for secure storage
    - Add functions for updating and deleting certificate comments
    - Optimize user permission verification logic

--- server/src/main/java/com/gregperlinli/certvault/service/interfaces/ICertificateService.java
+++ server/src/main/java/com/gregperlinli/certvault/service/interfaces/ICertificateService.java
@@ -1,16 +1,83 @@
 package com.gregperlinli.certvault.service.interfaces;
 
+import com.gregperlinli.certvault.domain.dto.*;
 import com.gregperlinli.certvault.domain.entities.Certificate;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
- * <p>
- * SSL证书 服务类
- * </p>
+ * SSL Certificate Service
  *
  * @author gregPerlinLi
  * @since 2025-03-03
  */
 public interface ICertificateService extends IService<Certificate> {
+
+    /**
+     * Get own SSL Certificates
+     *
+     * @param owner  Owner of the Certificate
+     * @param page   Page number
+     * @param limit  Number of certificates per page
+     * @return {@link PageDTO<CertificateDetailsDTO>}
+     */
+    PageDTO<CertInfoDTO> getCertificates(String owner, Integer page, Integer limit);
+
+    /**
+     * Get SSL Certificate
+     *
+     * @param uuid   Certificate UUID
+     * @param owner  Owner of the Certificate
+     * @return {@link String}
+     */
+    String getCertificateCert(String uuid, String owner);
+
+    /**
+     * Get SSL Certificate Private Key
+     *
+     * @param requestPrivkeyDTO Certificate Private Key Request DTO
+     * @param owner             Owner of the Certificate
+     * @return {@link String}
+     * @throws Exception Decryption error
+     */
+    String getCertificatePrivkey(RequestPrivkeyDTO requestPrivkeyDTO, String owner) throws Exception;
+
+    /**
+     * Update SSL Certificate Comment
+     *
+     * @param updateCommentDTO  Certificate Comment DTO
+     * @param owner             Owner of the Certificate
+     * @return {@link Boolean}
+     */
+    Boolean updateCertComment(UpdateCommentDTO updateCommentDTO, String owner);
+
+    /**
+     * Request a SSL Certificate
+     *
+     * @param requestCertDTO Certificate Request DTO
+     * @param owner          Owner of the Certificate
+     * @return {@link ResponseCertDTO}
+     * @throws Exception
+     */
+    ResponseCertDTO requestCert(RequestCertDTO requestCertDTO, String owner) throws Exception;
+
+    /**
+     * Renew a SSL Certificate
+     *
+     * @param oldCertUuid Certificate UUID
+     * @param expiry      Expiration time
+     * @param owner       Owner of the Certificate
+     * @return {@link ResponseCertDTO}
+     * @throws Exception e exception
+     */
+    ResponseCertDTO renewCert(String oldCertUuid, Integer expiry, String owner) throws Exception;
+
+    /**
+     * Delete a SSL Certificate
+     *
+     * @param uuid  Certificate UUID
+     * @param owner Owner of the Certificate
+     * @return {@link Boolean}
+     */
+    Boolean deleteCert(String uuid, String owner);
 
 }
