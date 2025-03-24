a64041b5c4d8db6a1c111d11680449f49bf8b370
李浩霖-gregPerlinLi- <lihaolin13@outlook.com>
2025-03-24 12:29:21 +0800

    feat(service): Add application version and timezone offset information
    - Add the applicationVersion field in TestServiceImpl to get the Spring application version
    - Add the getTimezoneOffset method to calculate the current timezone offset
    - Update the testGet, testPost, testPut, testDelete, and testPatch methods to add version number and timezone offset information to the returned TestDTO object

--- server/src/main/java/com/gregperlinli/certvault/service/impl/TestServiceImpl.java
+++ server/src/main/java/com/gregperlinli/certvault/service/impl/TestServiceImpl.java
@@ -2,9 +2,12 @@ package com.gregperlinli.certvault.service.impl;
 
 import com.gregperlinli.certvault.domain.dto.TestDTO;
 import com.gregperlinli.certvault.service.interfaces.ITestService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.GregorianCalendar;
 
 /**
  * Test Service Impl
@@ -17,34 +20,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestServiceImpl implements ITestService {
 
+    @Value("${spring.application.version}")
+    private String applicationVersion;
+
     @Override
     public TestDTO testGet() {
-        return new TestDTO(HttpMethod.GET.toString(), "Hello, This is CertVault SSL Certificate Management Platform");
+        return new TestDTO(HttpMethod.GET.toString(), "Hello, This is CertVault SSL Certificate Management Platform", applicationVersion, getTimezoneOffset());
     }
 
     @Override
     public TestDTO testPost() {
-        return new TestDTO(HttpMethod.POST.toString(), "Hello, This is CertVault SSL Certificate Management Platform");
+        return new TestDTO(HttpMethod.POST.toString(), "Hello, This is CertVault SSL Certificate Management Platform", applicationVersion, getTimezoneOffset());
     }
 
     @Override
     public TestDTO testPut() {
-        return new TestDTO(HttpMethod.PUT.toString(), "Hello, This is CertVault SSL Certificate Management Platform");
+        return new TestDTO(HttpMethod.PUT.toString(), "Hello, This is CertVault SSL Certificate Management Platform", applicationVersion, getTimezoneOffset());
     }
 
     @Override
     public TestDTO testDelete() {
-        return new TestDTO(HttpMethod.DELETE.toString(), "Hello, This is CertVault SSL Certificate Management Platform");
+        return new TestDTO(HttpMethod.DELETE.toString(), "Hello, This is CertVault SSL Certificate Management Platform", applicationVersion, getTimezoneOffset());
     }
 
     @Override
     public TestDTO testPatch() {
-        return new TestDTO(HttpMethod.PATCH.toString(), "Hello, This is CertVault SSL Certificate Management Platform");
+        return new TestDTO(HttpMethod.PATCH.toString(), "Hello, This is CertVault SSL Certificate Management Platform", applicationVersion, getTimezoneOffset());
     }
 
     @Override
     public String getBCryptedPassword(String password) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
         return encoder.encode(password);
+    }
+
+    private String getTimezoneOffset() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        int rawOffset = calendar.get(GregorianCalendar.ZONE_OFFSET);
+        return String.format("+%02d:%02d", rawOffset / 3600000, (rawOffset / 60000) % 60);
     }
 }
