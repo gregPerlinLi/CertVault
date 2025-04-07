35812348a740ede9bbd8ab71d12303fde3fbcc44
SNRainiar <rainiar@foxmail.com>
2025-04-07 09:48:04 +0800

    refactor(api): Reorganize user API module
    - Add `LoginRecordDTO` interface to `types.d.ts`
    - Delete `user/index.ts` and split its content into `user/user.ts` and `user/stats.ts`
    - Update imports in `stores/user.ts` to reflect new file structure
    - Introduce new API methods for user statistics and session management in `user/stats.ts` and `user/user.ts`

---
+++ frontend/src/api/user/user.ts
@@ -0,0 +1,45 @@
+import type { LoginRecordDTO, PaginationVO, UserProfileDTO } from "@/api/types";
+import { callRestfulApi } from "@/api";
+
+export const getProfile = () =>
+  callRestfulApi<UserProfileDTO>({
+    method: "GET",
+    baseUrl: "/api/v1/user/profile"
+  });
+
+export const getUsrLoginRecords = (
+  page: number,
+  limit: number,
+  status: number = -1
+) =>
+  callRestfulApi<PaginationVO<LoginRecordDTO>>({
+    method: "GET",
+    baseUrl: "/api/v1/user/session",
+    searchParams: { page, limit, status }
+  });
+
+export interface UpdateProfileRequestPayload {
+  displayName?: string;
+  email?: string;
+  oldPassword?: string;
+  newPassword?: string;
+}
+export const updateProfile = (payload: UpdateProfileRequestPayload) =>
+  callRestfulApi({
+    method: "PATCH",
+    baseUrl: "/api/v1/user/profile",
+    payload
+  });
+
+export const forceLogoutSession = (uuid: string) =>
+  callRestfulApi({
+    method: "DELETE",
+    baseUrl: "/api/v1/user/session/{uuid}/logout",
+    pathNames: { uuid }
+  });
+
+export const forceLogoutUsr = () =>
+  callRestfulApi({
+    method: "DELETE",
+    baseUrl: "/api/v1/user/logout"
+  });
