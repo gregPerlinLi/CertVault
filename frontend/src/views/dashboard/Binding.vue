c02c223fe62061a065c7c4dd0ccc207a75d44630
SNRainiar <rainiar@foxmail.com>
2025-04-22 17:02:39 +0800

    refactor(Binding.vue): Consolidate dialog state into a reactive object
    - Replace `dialogBind` with a `dialog` reactive object
    - Add `dialog.info` for CA info dialog
    - Update button click handlers to use the new `dialog` object
    - Add a new button to open the CA info dialog

--- frontend/src/views/dashboard/Binding.vue
+++ frontend/src/views/dashboard/Binding.vue
@@ -11,13 +11,16 @@ const refUsrTable = useTemplateRef("usr-table");
 
 /* Reactives */
 const busyUnbind = ref(false);
-const dialogBind = ref(false);
 const caSelection = ref<CaInfoDTO | null>(null);
 
 const usrTable = reactive({
   search: "",
   selection: [] as UserProfileDTO[],
   loading: false
+});
+const dialog = reactive({
+  bind: false,
+  info: false
 });
 
 /* Actions */
@@ -87,7 +90,7 @@ watch(caSelection, async () => {
           label="Bind"
           size="small"
           :disabled="caSelection === null || usrTable.loading"
-          @click="dialogBind = true"></Button>
+          @click="dialog.bind = true"></Button>
         <Button
           icon="pi pi-trash"
           label="Unbind Selected"
@@ -106,6 +109,13 @@ watch(caSelection, async () => {
           size="small"
           :disabled="caSelection === null || usrTable.loading"
           @click="refUsrTable?.refresh()"></Button>
+        <Button
+          icon="pi pi-info-circle"
+          label="CA Info"
+          severity="secondary"
+          size="small"
+          :disabled="caSelection === null"
+          @click="dialog.info = true"></Button>
       </div>
     </template>
     <template #end>
@@ -142,7 +152,11 @@ watch(caSelection, async () => {
 
   <!-- Dialogs -->
   <BindUsrsDlg
-    v-model:visible="dialogBind"
+    v-model:visible="dialog.bind"
     :ca="caSelection"
     @success="refUsrTable?.refresh()" />
+  <DispCertInfoDlg
+    v-model:visible="dialog.info"
+    variant="ca"
+    :data="caSelection ?? undefined" />
 </template>
